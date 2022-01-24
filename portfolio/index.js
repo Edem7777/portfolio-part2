c0c369cd9d79403932838b7d7726d29e81346376
Edem7777 <93734863+Edem7777@users.noreply.github.com>
2022-01-24 23:22:16 +0300

    portfolio-part2
    Так как не получилось задеплоить в приватный репозиторий школы (я из Крыма), по этой причине я создал в публичный репозитории весь проект первой части portfolio. Надеюсь на понимание.

---
+++ portfolio/index.js
@@ -0,0 +1,128 @@
+console.group('%cPortfolio', 'color: blue')
+console.log(
+  'Вёрстка валидная - выполнено полностью. ' + '%c+10',
+  'color: blue'
+);
+console.log(
+  'Header, main, footer - выполнено полностью. ' + '%c+2',
+  'color: blue'
+);
+console.log(
+  'Шесть элементов section (по количеству секций) - выполнено полностью. ' + '%c+2',
+  'color: blue'
+);
+console.log(
+  'Только один заголовок h1 - выполнено полностью. ' + '%c+2',
+  'color: blue'
+);
+console.log(
+  'Пять заголовков h2 (количество секций минус одна, у которой заголовок h1) - выполнено полностью. ' + '%c+2',
+  'color: blue'
+);
+console.log(
+  'Один элемент nav (панель навигации) - выполнено полностью. ' + '%c+2',
+  'color: blue'
+);
+console.log(
+  'Два списка ul > li > a (панель навигации, ссылки на соцсети) - выполнено полностью. ' + '%c+2',
+  'color: blue'
+);
+console.log(
+  'Десять кнопок button - выполнено полностью. ' + '%c+2',
+  'color: blue'
+);
+console.log(
+  'Два инпута: input type= email и input type= tel - выполнено полностью. ' + '%c+2',
+  'color: blue'
+);
+console.log(
+  'Один элемент textarea - выполнено полностью. ' + '%c+2',
+  'color: blue'
+);
+console.log(
+  'Три атрибута placeholder - выполнено полностью. ' + '%c+2',
+  'color: blue'
+);
+console.log(
+  'Блок header - выполнено полностью. ' + '%c+6',
+  'color: blue'
+);
+console.log(
+  'Секция hero - выполнено полностью. ' + '%c+6',
+  'color: blue'
+);
+console.log(
+  'Секция skills - выполнено полностью. ' + '%c+6',
+  'color: blue'
+);
+console.log(
+  'Секция portfolio - выполнено полностью. ' + '%c+6',
+  'color: blue'
+);
+console.log(
+    'Секция video - выполнено полностью. ' + '%c+6',
+    'color: blue'
+  );
+  console.log(
+    'Секция price - выполнено полностью. ' + '%c+6',
+    'color: blue'
+  );
+  console.log(
+    'Секция contacts - выполнено полностью. ' + '%c+6',
+    'color: blue'
+  );
+  console.log(
+    'Блок footer - выполнено полностью. ' + '%c+6',
+    'color: blue'
+  );
+  console.log(
+    'Для построения сетки используются флексы или гриды - выполнено полностью. ' + '%c+2',
+    'color: blue'
+  );
+  console.log(
+    'При уменьшении масштаба страницы браузера вёрстка размещается по центру - выполнено полностью. ' + '%c+2',
+    'color: blue'
+  );
+  console.log(
+    'Фоновый цвет тянется на всю ширину страницы - выполнено полностью. ' + '%c+2',
+    'color: blue'
+  );
+  console.log(
+    'Иконки добавлены в формате .svg - выполнено полностью. ' + '%c+2',
+    'color: blue'
+  );
+  console.log(
+    'Изображения добавлены в формате .jpg - выполнено полностью. ' + '%c+2',
+    'color: blue'
+  );
+  console.log(
+    'Есть favicon - выполнено полностью. ' + '%c+2',
+    'color: blue'
+  );
+  console.log(
+    'Плавная прокрутка по якорям - выполнено полностью. ' + '%c+5',
+    'color: blue'
+  );
+  console.log(
+    'Cсылки в футере ведут на гитхаб автора проекта и на страницу курса https://rs.school/js-stage0/. ' + '%c+5',
+    'color: blue'
+  );
+  console.log(
+    'Интерактивность включает в себя не только изменение внешнего вида курсора, но и другие визуальные эффекты. ' + '%c+5',
+    'color: blue'
+  );
+  console.log(
+    'Обязательное требование к интерактивности: плавное изменение внешнего вида элемента при наведении и клике не влияющее на соседние элементы. ' + '%c+5',
+    'color: blue'
+  );
+console.log(
+  '%c[Score: 110 / 110]',
+  'color: red'
+);
+console.log(
+ '%cGitHub - EDEM7777',
+  'color: green',
+);
+console.groupEnd()
+
+
